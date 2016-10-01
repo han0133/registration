19d0612322f1ccf3e3f0058a1e68f7fe247eaab8
soyoungahn <soyoungahn2706@gmail.com>
2016-10-01 12:38:04 +0900

    [안소영/1001] init

---
+++ singUp/src/main/java/com/singUp/SingUpApplication.java
@@ -0,0 +1,12 @@
+package com.singUp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SingUpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SingUpApplication.class, args);
+	}
+}
---
+++ singUp/src/test/java/com/singUp/SingUpApplicationTests.java
@@ -0,0 +1,16 @@
+package com.singUp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SingUpApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
